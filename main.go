8778ee00f3a391577848f7a07da90147b6f8c0b5
boke0 <boke0@boke0ya.com>
2022-06-28 12:55:01 +0900

    update

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	var users []User
 	var ratchets []Ratchet
-	for i := 0; i<1000; i++ {
+	for i := 0; i<100; i++ {
 		users = append(users, NewUser())
 	}
 	for _, user := range users {
@@ -22,29 +22,20 @@ func main() {
 		ratchet := NewRatchet(user.Id, ATGDHExchange, members)
 		ratchets = append(ratchets, ratchet)
 	}
-	fmt.Printf("count, enc_ticks, average_dec_ticks, max_ticks, min_ticks\n")
-	for i := 0; i<10000; i++ {
+	fmt.Printf("count, enc_ticks,\n")
+	for i := 0; i<1000; i++ {
 		enc_start := time.Now()
-		message := ratchets[i % 1000].Encrypt("hogehogehugahugapiyopiyo")
+		message := ratchets[i % 100].Encrypt("hogehogehugahugapiyopiyo")
 		enc_ticks := time.Since(enc_start).Microseconds()
-		fmt.Printf("%d, %d, ", i, enc_ticks)
-		var dec_a int64 = 0
-		var min int64 = 10000000000
-		var max int64 = 0
-		for j := 0; j<1000; j++ {
-			if (i % 1000) != j {
+		fmt.Printf("%d, %d", i, enc_ticks)
+		for j := 0; j<100; j++ {
+			if (i % 100) != j {
 				dec_start := time.Now()
 				ratchets[j].Decrypt(message)
 				dec_tick := time.Since(dec_start).Microseconds()
-				dec_a += dec_tick
-				if min > dec_tick {
-					min = dec_tick
-				}
-				if max < dec_tick {
-					max = dec_tick
-				}
+				fmt.Printf(", %d", dec_tick)
 			}
 		}
-		fmt.Printf("%d, %d, %d\n", dec_a / 999, max, min)
+		fmt.Printf("\n")
 	}
 }
