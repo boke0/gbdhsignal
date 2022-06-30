7a301deadca023cc4594f605338a2e61aa5a0100
boke0 <boke0@boke0ya.com>
2022-06-30 12:30:35 +0900

    update

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	var users []User
 	var ratchets []Ratchet
-	for i := 0; i<100; i++ {
+	for i := 0; i<500; i++ {
 		users = append(users, NewUser())
 	}
 	for _, user := range users {
@@ -23,13 +23,13 @@ func main() {
 		ratchets = append(ratchets, ratchet)
 	}
 	fmt.Printf("count, enc_ticks,\n")
-	for i := 0; i<1000; i++ {
+	for i := 0; i<10000; i++ {
 		enc_start := time.Now()
-		message := ratchets[i % 100].Encrypt("hogehogehugahugapiyopiyo")
+		message := ratchets[i % 500].Encrypt("hogehogehugahugapiyopiyo")
 		enc_ticks := time.Since(enc_start).Microseconds()
 		fmt.Printf("%d, %d", i, enc_ticks)
-		for j := 0; j<100; j++ {
-			if (i % 100) != j {
+		for j := 0; j<500; j++ {
+			if (i % 500) != j {
 				dec_start := time.Now()
 				ratchets[j].Decrypt(message)
 				dec_tick := time.Since(dec_start).Microseconds()
